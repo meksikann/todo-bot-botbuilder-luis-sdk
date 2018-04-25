6dacf9740d8c77c17933236c2be1771882de22bd
serhii <serhii.skoromets@eliftech.com>
2018-04-25 17:10:58 +0300

    setup bot dialog

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const restify = require('restify');
 const builder = require('botbuilder');
+let config = require('./config');
+let db = config.db;
 
 let server = restify.createServer();
+let regexpYes = /^yes$/i;
+let regexpNo = /^no$/i;
 
 //setup server
 server.listen(3978, () => {
@@ -20,22 +24,91 @@ let inMemoryStorage = new builder.MemoryBotStorage();
 // start set tasks conversation
 let bot = new builder.UniversalBot(connector,[
     function (session) {
-        session.send('Hi man, You do not have ay task scheduled. Lets add some.. :)');
-        session.beginDialog('askForTaskName');
+        session.send(
+            'Hi man. <br/> ' +
+            'Here are commands you can use:<br/>' +
+            '"add task" to create new task :)<br/>' +
+            '"show tasks" to get you task list.<br/>' +
+            'Have a nice journey!!!');
     }
 ]).set('storage', inMemoryStorage);
 
 
 //dialog to ask for a task name
-
 bot.dialog('askForTaskName',[
     (session) => {
-        builder.Prompt.text(session, "What is TODO name");
+        builder.Prompts.text(session, "What is new task name?");
+    },
+    async (session, results, next) => {
+        let todo = {
+            title: results.response,
+            userId: session.message.user.id
+        };
+
+        try {
+            const result = await db.todos.save(todo);
+            session.send(`Ok, I've saved your task :"${results.response}" in my database.`);
+            next();
+        } catch(e) {
+            console.error(e);
+            session.send(`Ups!! Cannot save "${results.response}" in my database... Please try again`);
+            session.endDialog();
+        }
+    },
+    (session) => {
+        builder.Prompts.text(session, 'do you want to add one more? "yes/no"? ');
     },
     (session, results) => {
-        session.endDialogWithResult(results);
-
+        if(results && results.response.match(regexpYes)) {
+            session.beginDialog('askForTaskName');
+        } else {
+            session.send('Ok. no problem.');
+            session.endDialog();
+        }
     }
-]);
+]).triggerAction({
+    matches: /^add task$/i,
+});
+
+
+//dialog to get tasks
+bot.dialog('getTodos',[
+    async (session) => {
+        const findQuery = {'userId':session.message.user.id};
+        let todosResponse='';
+        let textMessage;
+
+        try {
+            let todos = await db.collection('todos').findAsCursor(findQuery).toArray();
+
+            if(todos && todos.length) {
+                todosResponse = getFormatedTodos(todos);
+                textMessage = `Here are your task(s):<br/> ${todosResponse}`
+            } else {
+                textMessage = 'You do not have any task scheduled';
+            }
+
+            session.send(textMessage);
+
+        } catch(e) {
+            console.error(e);
+            session.send(`Ups!! Cannot save "${results.response}" in my database... Please try again`);
+        }
+
+        session.endDialogWithResult();
+    }
+]).triggerAction({
+    matches: /^show tasks$/i,
+});
+
+function getFormatedTodos (todos) {
+    let todosResponse='';
+
+    todos.forEach(todo => {
+        todosResponse += `- ${todo.title} <br/>`;
+    });
+
+    return todosResponse;
+}
 
 
--- config/index.js
+++ config/index.js
@@ -1,5 +1,8 @@
+let mongoist  = require('mongoist');
+
 let config = {
-    luisUrl: 'https://westeurope.api.cognitive.microsoft.com/luis/v2.0/apps/'
+    luisUrl: 'https://westeurope.api.cognitive.microsoft.com/luis/v2.0/apps/',
+    db: mongoist('mongodb://127.0.0.1:27017/todobot')
 };
 
 module.exports = config;
